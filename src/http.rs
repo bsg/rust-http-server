7718385415a45c0ddb46b32f90b19ef2a107391b
Cem Saldirim <cem.saldirim@gmail.com>
2022-09-24 16:11:32 +0300

    Refactored HttpRequest. Moved response to a fn

--- src/http.rs
+++ src/http.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 #[derive(Debug)]
 pub struct HttpHeader {
     pub length: usize,
+    pub method: String,
     pub uri: String,
     pub version: String,
     pub headers: HashMap<String, String>,
@@ -24,8 +25,8 @@ impl HttpRequest {
     pub fn parse(buffer: &str) -> HttpParseResult {
         match buffer.find("\r\n\r\n") {
             Some(len) => {
-                let mut request_type = "";
                 let mut header = HttpHeader {
+                    method: "".to_string(),
                     length: len + 4,
                     uri: "".to_string(),
                     version: "".to_string(),
@@ -38,24 +39,24 @@ impl HttpRequest {
                         let mut it = line.split(|c: char| c == ' ');
                         match it.next() {
                             Some(s) => {
-                                request_type = s;
+                                header.method = s.to_string();
                             }
-                            None => return HttpParseResult::Err("Malformed header".to_string()),
+                            None => return HttpParseResult::Err("Malformed request".to_string()),
                         }
                         match it.next() {
                             Some(s) => {
                                 header.uri = s.to_string();
                             }
-                            None => return HttpParseResult::Err("Malformed header".to_string()),
+                            None => return HttpParseResult::Err("Malformed request".to_string()),
                         }
                         match it.next() {
                             Some(s) => {
                                 header.version = s.to_string();
                             }
-                            None => return HttpParseResult::Err("Malformed header".to_string()),
+                            None => return HttpParseResult::Err("Malformed request".to_string()),
                         }
                     }
-                    None => return HttpParseResult::Err("Malformed header".to_string()),
+                    None => return HttpParseResult::Err("Malformed request".to_string()),
                 }
 
                 for line in lines {
@@ -83,12 +84,12 @@ impl HttpRequest {
                     header.headers.insert(key, value);
                 }
 
-                if request_type == "GET" {
+                if header.method == "GET" {
                     HttpParseResult::Ok(HttpRequest::GET(header))
-                } else if request_type == "POST" {
+                } else if header.method == "POST" {
                     HttpParseResult::Ok(HttpRequest::POST(header))
                 } else {
-                    HttpParseResult::Err("Incorrect request type".to_string())
+                    HttpParseResult::Err("Incorrect request method".to_string())
                 }
             }
             None => HttpParseResult::Incomplete,
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod http;
-use std::time::SystemTime;
-use chrono::offset::Utc;
-use chrono::DateTime;
 use ascii::AsciiStr;
 use ascii::AsciiString;
+use chrono::offset::Utc;
+use chrono::DateTime;
+use http::HttpHeader;
 use http::HttpParseResult;
+use std::time::SystemTime;
 use tokio::io;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
@@ -27,7 +28,7 @@ async fn main() {
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(mut socket: TcpStream) {
     let mut chunk: [u8; 1024] = [0; 1024];
     let mut buf = AsciiString::with_capacity(1024 * 16);
 
@@ -38,33 +39,41 @@ async fn process(socket: TcpStream) {
             Ok(n) => {
                 buf.push_str(AsciiStr::from_ascii(&chunk[0..n]).unwrap());
                 match http::HttpRequest::parse(&buf.as_str()) {
-                    HttpParseResult::Ok(header) => {
-                        match header {
-                            http::HttpRequest::GET(header) => {
-                                dbg!(header);
-                                let now = SystemTime::now();
-                                let datetime: DateTime<Utc> = now.into();
-                                let content = datetime.format("%d/%m/%Y %T").to_string();
-                                let response = format!("HTTP 200 OK\r\nContent-Length: {}\r\n\r\n{}", content.len(), content);
-                                socket.try_write(response.as_bytes()).unwrap();
-                                buf.clear();
-                            }
-                            http::HttpRequest::POST(_) => (),
+                    HttpParseResult::Ok(header) => match header {
+                        http::HttpRequest::GET(header) => {
+                            let header = dbg!(header);
+                            socket = respond_time(socket, header);
+                            buf.clear();
                         }
+                        http::HttpRequest::POST(_) => (),
                     },
                     HttpParseResult::Incomplete => continue,
                     HttpParseResult::Err(e) => {
                         println!("{e:?}");
-                    },
+                    }
                 }
             }
             Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
                 continue;
             }
             Err(e) => {
+                println!("{e:?}");
                 break;
             }
         }
-        
     }
+}
+
+fn respond_time(socket: TcpStream, header: HttpHeader) -> TcpStream {
+    let now = SystemTime::now();
+    let datetime: DateTime<Utc> = now.into();
+    let mut content = datetime.format("%d/%m/%Y %T").to_string();
+    content.push_str(format!("\n\n{:#?}", header).as_str());
+    let response = format!(
+        "HTTP 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        content.len(),
+        content
+    );
+    socket.try_write(response.as_bytes()).unwrap();
+    socket
 }
