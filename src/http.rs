6305ef19b5f46ff25121c5979ffd7fb2b430eee5
Cem Saldirim <cem.saldirim@gmail.com>
2022-09-25 00:19:48 +0300

    Rewritten HttpRequest as a stream consumer.

--- src/http.rs
+++ src/http.rs
@@ -1,8 +1,10 @@
 use std::collections::HashMap;
+use tokio::io::AsyncBufReadExt;
+use tokio::io::BufReader;
+use tokio::net::tcp::ReadHalf;
 
 #[derive(Debug)]
 pub struct HttpHeader {
-    pub length: usize,
     pub method: String,
     pub uri: String,
     pub version: String,
@@ -17,82 +19,109 @@ pub enum HttpRequest {
 
 pub enum HttpParseResult {
     Ok(HttpRequest),
-    Incomplete,
     Err(String),
 }
 
 impl HttpRequest {
-    pub fn parse(buffer: &str) -> HttpParseResult {
-        match buffer.find("\r\n\r\n") {
-            Some(len) => {
-                let mut header = HttpHeader {
-                    method: "".to_string(),
-                    length: len + 4,
-                    uri: "".to_string(),
-                    version: "".to_string(),
-                    headers: HashMap::new(),
-                };
+    pub async fn from_stream(
+        stream: BufReader<ReadHalf<'_>>,
+    ) -> (BufReader<ReadHalf<'_>>, HttpParseResult) {
+        let mut header = HttpHeader {
+            method: "".to_string(),
+            uri: "".to_string(),
+            version: "".to_string(),
+            headers: HashMap::new(),
+        };
 
-                let mut lines = buffer.lines();
-                match lines.next() {
+        let mut lines = stream.lines();
+        match lines.next_line().await {
+            Ok(o) => match o {
+                Some(line) => {
+                    let mut it = line.split(|c: char| c == ' ');
+                    match it.next() {
+                        Some(s) => {
+                            header.method = s.to_string();
+                        }
+                        None => return (lines.into_inner(), HttpParseResult::Err("".to_string())),
+                    }
+                    match it.next() {
+                        Some(s) => {
+                            header.uri = s.to_string();
+                        }
+                        None => return (lines.into_inner(), HttpParseResult::Err("".to_string())),
+                    }
+                    match it.next() {
+                        Some(s) => {
+                            header.version = s.to_string();
+                        }
+                        None => return (lines.into_inner(), HttpParseResult::Err("".to_string())),
+                    }
+                }
+                None => return (lines.into_inner(), HttpParseResult::Err("".to_string())),
+            },
+            Err(_e) => {
+                return (
+                    lines.into_inner(),
+                    HttpParseResult::Err("IO error".to_string()),
+                );
+            }
+        }
+
+        loop {
+            match lines.next_line().await {
+                Ok(o) => match o {
                     Some(line) => {
-                        let mut it = line.split(|c: char| c == ' ');
-                        match it.next() {
-                            Some(s) => {
-                                header.method = s.to_string();
+                        if line.is_empty() {
+                            break;
+                        } else {
+                            let mut it = line.splitn(2, ": ");
+                            match it.next() {
+                                Some(k) => match it.next() {
+                                    Some(v) => {
+                                        header.headers.insert(k.to_string(), v.to_string());
+                                    }
+                                    None => {
+                                        return (
+                                            lines.into_inner(),
+                                            HttpParseResult::Err("Malformed header".to_string()),
+                                        );
+                                    }
+                                },
+                                None => {
+                                    return (
+                                        lines.into_inner(),
+                                        HttpParseResult::Err("Malformed header".to_string()),
+                                    );
+                                }
                             }
-                            None => return HttpParseResult::Err("Malformed request".to_string()),
-                        }
-                        match it.next() {
-                            Some(s) => {
-                                header.uri = s.to_string();
-                            }
-                            None => return HttpParseResult::Err("Malformed request".to_string()),
-                        }
-                        match it.next() {
-                            Some(s) => {
-                                header.version = s.to_string();
-                            }
-                            None => return HttpParseResult::Err("Malformed request".to_string()),
                         }
                     }
-                    None => return HttpParseResult::Err("Malformed request".to_string()),
-                }
-
-                for line in lines {
-                    let key: String;
-                    let value: String;
-
-                    let mut it = line.split(": ");
-                    match it.next() {
-                        Some(s) => {
-                            key = s.to_string();
-                        }
-                        None => {
-                            continue;
-                        }
-                    }
-                    match it.next() {
-                        Some(s) => {
-                            value = s.to_string();
-                        }
-                        None => {
-                            continue;
-                        }
-                    }
-
-                    header.headers.insert(key, value);
-                }
-
-                if header.method == "GET" {
-                    HttpParseResult::Ok(HttpRequest::GET(header))
-                } else if header.method == "POST" {
-                    HttpParseResult::Ok(HttpRequest::POST(header))
-                } else {
-                    HttpParseResult::Err("Incorrect request method".to_string())
+                    None => continue,
+                },
+                Err(_e) => {
+                    return (
+                        lines.into_inner(),
+                        HttpParseResult::Err("IO error".to_string()),
+                    );
                 }
             }
-            None => HttpParseResult::Incomplete,
+        }
+
+        if header.method == "GET" {
+            (
+                lines.into_inner(),
+                HttpParseResult::Ok(HttpRequest::GET(header)),
+            )
+        } else if header.method == "POST" {
+            (
+                lines.into_inner(),
+                HttpParseResult::Ok(HttpRequest::POST(header)),
+            )
+        } else {
+            (
+                lines.into_inner(),
+                HttpParseResult::Err("Method not supported".to_string()),
+            )
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,85 @@
 mod http;
-use ascii::AsciiStr;
-use ascii::AsciiString;
 use chrono::offset::Utc;
 use chrono::DateTime;
 use http::HttpHeader;
 use http::HttpParseResult;
+use http::HttpRequest;
 use std::time::SystemTime;
-use tokio::io;
+use tokio::io::AsyncWriteExt;
+use tokio::io::BufReader;
+use tokio::io::BufWriter;
+use tokio::net::tcp::ReadHalf;
+use tokio::net::tcp::WriteHalf;
 use tokio::net::TcpListener;
 use tokio::net::TcpStream;
+use tokio::runtime;
 
-#[tokio::main]
-async fn main() {
+fn main() {
     #[cfg(debug_assertions)]
     println!("Debug enabled");
 
-    let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
-    println!("Listening");
+    //let rt = runtime::Builder::new_current_thread().enable_all().build().unwrap();
+    let rt = runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
 
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        println!("Accepted");
+    rt.block_on(async {
+        let listener = TcpListener::bind("127.0.0.1:8000").await.unwrap();
+        println!("Listening");
 
-        tokio::spawn(async move {
-            process(socket).await;
-        });
-    }
+        loop {
+            let (socket, _) = listener.accept().await.unwrap();
+            println!("Accepted");
+
+            tokio::spawn(async move {
+                process(socket).await;
+            });
+        }
+    });
 }
 
 async fn process(mut socket: TcpStream) {
-    let mut chunk: [u8; 1024] = [0; 1024];
-    let mut buf = AsciiString::with_capacity(1024 * 16);
+    let (socket_rx, socket_tx) = socket.split();
+    let mut rx = BufReader::new(socket_rx);
+    let mut tx = BufWriter::new(socket_tx);
 
     loop {
-        match socket.readable().await {
-            Ok(_) => match socket.try_read(&mut chunk) {
-                Ok(0) => break,
-                Ok(n) => {
-                    buf.push_str(AsciiStr::from_ascii(&chunk[0..n]).unwrap());
-                    match http::HttpRequest::parse(&buf.as_str()) {
-                        HttpParseResult::Ok(header) => match header {
-                            http::HttpRequest::GET(header) => {
-                                let header = dbg!(header);
-                                socket = respond_time(socket, header);
-                                buf.clear();
-                            }
-                            http::HttpRequest::POST(_) => (),
-                        },
-                        HttpParseResult::Incomplete => continue,
-                        HttpParseResult::Err(e) => {
-                            println!("{e:?}");
-                        }
-                    }
+        let request;
+        (rx, request) = HttpRequest::from_stream(rx).await;
+        match request {
+            HttpParseResult::Ok(request) => match request {
+                HttpRequest::GET(header) => {
+                    println!("GET {}", header.uri);
+                    (rx, tx) = respond_time(rx, tx, header).await;
+                    tx.flush().await;
                 }
-                Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                    continue;
-                }
-                Err(e) => {
-                    println!("{e:?}");
-                    break;
+                HttpRequest::POST(header) => {
+                    println!("POST {}", header.uri);
                 }
             },
-            Err(e) => println!("{e:?}"),
+            HttpParseResult::Err(e) => {
+                println!("{e:?}");
+                break;
+            }
         }
     }
 }
 
-fn respond_time(socket: TcpStream, header: HttpHeader) -> TcpStream {
+async fn respond_time<'a>(
+    rx: BufReader<ReadHalf<'a>>,
+    mut tx: BufWriter<WriteHalf<'a>>,
+    header: HttpHeader,
+) -> (BufReader<ReadHalf<'a>>, BufWriter<WriteHalf<'a>>) {
     let now = SystemTime::now();
     let datetime: DateTime<Utc> = now.into();
     let mut content = datetime.format("%d/%m/%Y %T UTC").to_string();
     content.push_str(format!("\n\n{:#?}", header).as_str());
     let response = format!(
-        "HTTP 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        "HTTP 200 OK\r\nConnection: keep-alive\r\nContent-Length: {}\r\n\r\n{}",
         content.len(),
         content
     );
-    socket.try_write(response.as_bytes()).unwrap();
-    socket
+    tx.write_all(response.as_bytes()).await;
+    (rx, tx)
 }
